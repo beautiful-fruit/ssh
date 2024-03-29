de17aea80b7c236891312f91dca65196b3685a97
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2024-03-30 00:32:45 +0800

    Auto Commit

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-from discord import Bot, SlashCommandGroup
+from discord import Bot, ApplicationContext
 
-from asyncio import AbstractEventLoop
+from asyncio import AbstractEventLoop, open_connection
 from typing import Optional
 
 from config import TOKEN
@@ -12,6 +12,17 @@ bot.load_extension("cogs.system")
 @bot.event
 async def on_ready():
     print(f"{bot.user.display_name} has connected to discord.")
+
+
+@bot.slash_command("call")
+async def call(ctx: ApplicationContext):
+    try:
+        _, w = await open_connection("100.0.0.110", 8888, timeout=1)
+        w.close()
+        await ctx.respond("Send success.")
+    except:
+        await ctx.respond("Send failed.")
+
 
 
 def start(loop: Optional[AbstractEventLoop] = None):
